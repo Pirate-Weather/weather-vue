5801c645de46c3b8029c0f2bd9f1419955c90fb9
Alexander Rey <alexander@Voyager.alexanderrey.ca>
2021-05-04 19:47:08 -0400

    Updated Names

--- src/components/Credits.vue
+++ src/components/Credits.vue
@@ -3,13 +3,13 @@
     <div>
       <span>Weather Vue is built with</span>
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue.js</a>,
-      <a href="https://darksky.net/dev/" target="_blank" rel="noopener">Dark Sky API</a>,
+      <a href="https://pirateweather.net" target="_blank" rel="noopener">Pirate Weather API</a>,
       <a href="https://developers.google.com/maps/documentation/javascript/get-api-key" target="_blank" rel="noopener">Google Maps JavaScript API</a>,
       <a href="https://developers.google.com/maps/documentation/geocoding/get-api-key" target="_blank" rel="noopener">Google Geocoding API</a>,
       <a href="https://developers.google.com/places/web-service/autocomplete" target="_blank" rel="noopener">Google Places API</a>,
       and many other great packages.
       <p class="source">
-        Grab the <a href="https://github.com/krestaino/weather-vue/" target="_blank" rel="noopener">source</a>.
+        Grab the <a href="https://github.com/alexander0042/weather-vue/" target="_blank" rel="noopener">source</a>.
       </p>
     </div>
   </div>
